336696cc82a9ab4a0021dbcb7e87ae0a00f41485
gelleouet <gregory.elleouet@gmail.com>
2017-03-23 12:32:07 +0100

    Connexion du shell uniquement à la demande

--- Shell.js
+++ Shell.js
@@ -5,7 +5,7 @@
  * 
  * @author gregory.elleouet@gmail.com
  */
-var pty = require('pty.js');
+var pty = require('pty');
 var Device = require("./Device").Device;
 
 
@@ -14,21 +14,7 @@ var Device = require("./Device").Device;
  */
 var Shell = function Shell(server) {
 	this.server = server
-	
-	this.xterm = pty.spawn('bash', [], {
-	  name: 'xterm-color',
-	  cols: 80,
-	  rows: 30,
-	  cwd: process.env.HOME,
-	  env: process.env
-	})
-	
-	// a chaque réception de data, on revoit le tout au serveur principal
-	this.xterm.on('data', function(data) {
-		var device = new Device('xterm-color', true, server)
-		device.value = data
-		server.emit('value', device, 'shell')
-	});
+	this.xterm = null
 };
 
 
@@ -37,8 +23,16 @@ var Shell = function Shell(server) {
  * 
  * @param data
  */
-Log.prototype.write = function(data) {
-	this.xterm.write(data)
+Shell.prototype.write = function(data) {
+	if (data == "close") {
+		this.free()
+	} else {
+		// on vérifie si déjà connecté
+		if (! this.xterm) {
+			this.connect()
+		}
+		this.xterm.write(data)
+	}
 };
 
 
@@ -47,8 +41,32 @@ Log.prototype.write = function(data) {
  * 
  * @param data
  */
-Log.prototype.free = function() {
+Shell.prototype.free = function() {
 	this.xterm.destroy()
+	this.xterm = null
+};
+
+
+/**
+ * Arrêt du shell
+ * 
+ * @param data
+ */
+Shell.prototype.connect = function() {
+	this.xterm = pty.spawn('bash', [], {
+	  name: 'xterm-color',
+	  cols: 80,
+	  rows: 30,
+	  cwd: process.env.HOME,
+	  env: process.env
+	})
+	
+	// a chaque réception de data, on renvoit le tout au serveur principal
+	this.xterm.on('data', function(data) {
+		var device = new Device('xterm-color', true, server)
+		device.value = data
+		server.emit('value', device, 'shell')
+	});
 };
 
 
