c771c3e1b7e887c90e336e42a1e6217f81bdf1d9
gelleouet <gregory.elleouet@gmail.com>
2018-04-19 22:19:56 +0200

    Debug bme280

--- i2c/bme280test.js
+++ i2c/bme280test.js
@@ -1,22 +1,24 @@
-var i2c = require('../I2C').I2C();
-var deviceServer = require('./DeviceServer').newInstance();
+var I2C = require('../I2C').I2C;
+var deviceServer = require('../DeviceServer').newInstance();
 
 // gestionnaire fin application
 process.on('SIGINT', exit);
 process.on('SIGTERM', exit);
 
-var i2cDriver = new I2C(deviceServer, {
+
+var i2cDriver = new I2C(deviceServer)
+i2cDriver.credentials = {
 	i2c: {
 		bus: 1,
 		bme280: {
 			address: 119
 		}
 	}
-})
-
+}
 i2cDriver.init()
 
+
 function exit() {
-	deviceServer.close();
 	i2cDriver.free();
+	process.exit();
 }
